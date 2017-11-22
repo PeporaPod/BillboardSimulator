cd4ec3d4f6406547d79a05bd6e4f0093fd6e1ecb
ぺぽらポッド <peporapod01@icloud.com>
2017-11-23 01:14:24 +0900

    000

--- BillboardSimulator/BillboardSimulator/StringFile.cpp
+++ BillboardSimulator/BillboardSimulator/StringFile.cpp
@@ -8,12 +8,50 @@ StringFile::StringFile()
 
 void StringFile::Init()
 {
-	for (int i = 1; LoadBinaryString(i); i++);
+	for (int i = 1; LoadBinaryString(i); i++) count++;
+	singularity();
 }
 
 
 StringFile::~StringFile()
 {
+}
+
+void StringFile::singularity()
+{
+	std::ifstream file;
+	file.open("0.seg");
+	if (file.is_open()) {
+		StringInformation strinfo;
+		for (int row = 0; row < LED_ROW; row++) {
+			std::string str;
+			std::getline(file, str);
+			unsigned long long led_array = 0;
+			unsigned long long operatorbit = 0x8000000000000000;
+			for (int column = 0; (unsigned int)column < str.size() && column < LED_COLUMN; row++) {
+				if (str[column] = '1')
+					led_array = led_array & operatorbit;
+				operatorbit = operatorbit >> 1;
+			}
+			strinfo.led_status[row] = led_array;
+		}
+		stringlist.push_back(strinfo);
+		file.close();
+	}
+	file.open("0", std::fstream::binary);
+	if (file.is_open()) {
+		StringInformation strinfo;
+		file.read(strinfo.string, sizeof(StringInformation::string));
+		file.read((char*)&strinfo.width, sizeof(StringInformation::width));
+		file.read(&strinfo.type, sizeof(StringInformation::type));
+		file.read((char*)&strinfo.R, sizeof(StringInformation::R));
+		file.read((char*)&strinfo.G, sizeof(StringInformation::G));
+		file.read((char*)&strinfo.B, sizeof(StringInformation::B));
+		for (int row = 0; row < LED_ROW; row++)
+			file.read((char*)&strinfo.led_status[row], sizeof(unsigned long long));
+		stringlist.push_back(strinfo);
+		file.close();
+	}
 }
 
 bool StringFile::LoadBinaryString(int filenumber)
@@ -35,4 +73,25 @@ bool StringFile::LoadBinaryString(int filenumber)
 		file.read((char*)&strinfo.led_status[row], sizeof(unsigned long long));
 	stringlist.push_back(strinfo);
 	return true;
+}
+
+void StringFile::Assort()
+{
+	while (!stringlist.empty()) {
+		switch (stringlist.front().type) {
+		case 'J':
+		case 'E':
+			stringvector.push_back(stringlist.front());
+			stringlist.pop_front();
+			break;
+		case 'N':
+			numberstring.push_back(stringlist.front());
+			stringlist.pop_front();
+			break;
+		default:
+			symbolstring.push_back(stringlist.front());
+			stringlist.pop_front();
+			break;
+		}
+	}
 }
--- BillboardSimulator/BillboardSimulator/StringFile.h
+++ BillboardSimulator/BillboardSimulator/StringFile.h
@@ -2,9 +2,11 @@
 #include "Header.h"
 #include <fstream>
 #include <list>
+#include <string>
 class StringFile
 {
 private:
+	int count = 0;
 	std::list<StringInformation>	stringlist;
 	std::vector<StringInformation>	numberstring;
 	std::vector<StringInformation>	symbolstring;
@@ -14,6 +16,8 @@ public:
 	void Init();
 	~StringFile();
 private:
+	void singularity();
 	bool LoadBinaryString(int filenumber);
+	void Assort();
 };
 
